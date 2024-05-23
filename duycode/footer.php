e979a31b0afb200ce681bbbe2599faf0a77b5c10
duyk30b <duyk30b@gmail.com>
2024-05-23 11:07:29 +0700

    create 1 file and update 4 files

--- duycode/footer.php
+++ duycode/footer.php
@@ -4,24 +4,6 @@
     <span> DuyCode &copy; 2019. Website được phát triển bởi công ty TNHH Medihome - 0986.021.190 </span>
   </div>
 </footer>
-<script>
-document.querySelectorAll("pre").forEach((pre) => {
-  pre.innerHTML = pre.innerHTML.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-});
-</script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-javascript.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-typescript.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-css.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-html.min.js"></script>
-<style>
-  pre[class*=language-] {
-    padding: 0 1em 1em 1em;
-    margin: 0;
-    overflow: auto;
-  }
-</style>
 </body>
 
 </html>
--- duycode/functions.php
+++ duycode/functions.php
@@ -2,6 +2,9 @@
 # Use classic editor
 add_filter('use_block_editor_for_post', '__return_false');
 
+# Don't replace " " to ” ”
+remove_filter('the_content', 'wptexturize');
+
 # Displays post image attachment (sizes: thumbnail, medium, full)
 function get_first_image() {
    global $post;
@@ -23,5 +26,22 @@ function get_first_image() {
    return $first_img;
 }
 
-#Xoa dau ngoac kep cong
-remove_filter('the_content', 'wptexturize');
+function replace_pre_and_code_content($content) {
+   // Định nghĩa biểu thức chính quy để tìm và thay thế nội dung trong thẻ <pre><code>
+   $pattern = '/<pre><code(.*?)>(.*?)<\/code><\/pre>/s';
+
+   // Hàm preg_replace_callback sẽ thay thế mỗi kết quả của biểu thức chính quy bằng nội dung đã được xử lý
+   $safe_content = preg_replace_callback($pattern, function($matches) {
+       // $matches[2] chứa nội dung bên trong thẻ <pre><code>
+       $code_content = $matches[2];
+       // Thực hiện thay đổi ký tự trong nội dung bên trong thẻ <pre><code>
+       $code_content = str_replace('<', '&lt;', $code_content);
+       $code_content = str_replace('>', '&gt;', $code_content);
+       // Trả về nội dung đã được thay đổi
+       return '<pre><code' . $matches[1] . '>' . $code_content . '</code></pre>';
+   }, $content);
+
+   // Trả về nội dung bài viết với các thay đổi
+   return $safe_content;
+}
+add_filter('the_content', 'replace_pre_and_code_content');
